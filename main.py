58acc86a81a9fc93047bb3e587f38fe76a67b7c3
Ritobrata Ghosh <58001482+ghosh-r@users.noreply.github.com>
2021-07-16 00:27:58 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Demo for DALL-E Mini")
+
+st.write("This is a demo for DALL-E Mini, an Open Source AI model that generates images from nothing but a text prompt")
+
+
